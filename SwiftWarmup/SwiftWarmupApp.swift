17e36b2fa96a9dc9e2d088b44a5a3a8c52a6814d
Josh Jo <liebenholz98@gmail.com>
2024-01-15 10:12:58 +0900

    Initial Commit

---
+++ SwiftWarmup/SwiftWarmupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWarmupApp.swift
+//  SwiftWarmup
+//
+//  Created by gokyulueau on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWarmupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
