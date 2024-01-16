3b673d2feb3ab47100834ad8044b51079cdd9fca
Josh Jo <liebenholz98@gmail.com>
2024-01-16 12:04:04 +0900

    3-3 Tab View Edited

---
+++ SwiftWarmup/03/MyTab2.swift
@@ -0,0 +1,18 @@
+//
+//  MyTab2.swift
+//  SwiftWarmup
+//
+//  Created by gokyulueau on 1/16/24.
+//
+
+import SwiftUI
+
+struct MyTab2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyTab2()
+}
